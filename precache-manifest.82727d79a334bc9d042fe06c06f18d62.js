2b850dbcaf3b90921b0460654d765972fa74b2d0
Ricardo Gonzalez Villegas <rikarudogonzarezu@gmail.com>
2020-06-10 16:25:31 -0400

    Updates

---
+++ precache-manifest.82727d79a334bc9d042fe06c06f18d62.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93f12ad727f2e84c2ec7e8b48f09d3d",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "1f01c18f345cc5853b6b",
+    "url": "/weather-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8abe1f7b79153bf6162d",
+    "url": "/weather-react-app/static/js/2.1b5125ba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/weather-react-app/static/js/2.1b5125ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f01c18f345cc5853b6b",
+    "url": "/weather-react-app/static/js/main.58ab6721.chunk.js"
+  },
+  {
+    "revision": "603961fde81256cc2eab",
+    "url": "/weather-react-app/static/js/runtime-main.96a07b75.js"
+  },
+  {
+    "revision": "0a5cc38edb5c31370d219e0daeedd1a4",
+    "url": "/weather-react-app/static/media/clouds.0a5cc38e.svg"
+  },
+  {
+    "revision": "67b5ea5ac4f0a9ae52c442e7e160ae8c",
+    "url": "/weather-react-app/static/media/high.67b5ea5a.svg"
+  },
+  {
+    "revision": "0d0e5a68f7100f1e578e8df073037f06",
+    "url": "/weather-react-app/static/media/humidity.0d0e5a68.svg"
+  },
+  {
+    "revision": "d9a5c3ae99eb9ddb6fb666b4717c0c20",
+    "url": "/weather-react-app/static/media/low.d9a5c3ae.svg"
+  },
+  {
+    "revision": "89b6cd13669285d59c9f5e073c86deb2",
+    "url": "/weather-react-app/static/media/mist.89b6cd13.svg"
+  },
+  {
+    "revision": "5e52afe2c2c4306992c8dd1cb174471c",
+    "url": "/weather-react-app/static/media/rain.5e52afe2.svg"
+  },
+  {
+    "revision": "fa2da37fd3a10211e4590f729d012ad8",
+    "url": "/weather-react-app/static/media/shower.fa2da37f.svg"
+  },
+  {
+    "revision": "bf6fc3a8dbe3b5c853e79407fb2c9a4c",
+    "url": "/weather-react-app/static/media/snow.bf6fc3a8.svg"
+  },
+  {
+    "revision": "1f6a4e37f71269443a6671139008ba58",
+    "url": "/weather-react-app/static/media/sunny.1f6a4e37.svg"
+  },
+  {
+    "revision": "dcde16ad8d3ed8f1753fb6404587c94c",
+    "url": "/weather-react-app/static/media/thunderstorm.dcde16ad.svg"
+  },
+  {
+    "revision": "0b616cd4ea4f11348a7fe33382f419e2",
+    "url": "/weather-react-app/static/media/wind.0b616cd4.svg"
+  }
+]);
